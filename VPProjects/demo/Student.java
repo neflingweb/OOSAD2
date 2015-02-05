4bcaecbb8f3eb9f3bf257c104ca2f32aebb62b51
Nefling <neflingweb@gmail.com>
2015-02-05 12:21:02 +0000

    week2

---
+++ VPProjects/demo/Student.java
@@ -0,0 +1,30 @@
+package demo;
+
+public class Student {
+
+	private string name;
+	private string address;
+	private string id;
+	private string mobile;
+	private string dob;
+
+	public void getName() {
+		// TODO - implement Student.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setName(int attribute) {
+		// TODO - implement Student.setAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	public Student() {
+		// TODO - implement Student.Student
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ week2/org/Week2/Cart.java
@@ -0,0 +1,14 @@
+package org.Week2;
+
+import java.util.*;
+
+public class Cart {
+
+	public Cart() {
+		// TODO - implement Cart.Cart
+		throw new UnsupportedOperationException();
+	}
+
+	protected Collection<CartItem> cartitem;
+
+}
